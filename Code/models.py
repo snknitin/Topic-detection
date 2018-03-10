b1cf8678ca8ebecc4a97af144faeb2bb11e862f2
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2018-03-10 17:07:15 -0500

    Changing print statements

--- Code/models.py
+++ Code/models.py
@@ -1,5 +1,5 @@
 """Shared inputs and interfaces for topic models."""
-
+import time
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 import shared_utils as su
@@ -17,9 +17,14 @@ def runLDA(documents,vocab_size,numTopics):
     tf_feature_names = tf_vectorizer.get_feature_names()
 
     # Running
-    print("Running LDA")
+    print("Running Latent Dirichlet Allocation")
+    t0 = time.time()
     lda = LatentDirichletAllocation(n_topics=numTopics, max_iter=5, learning_method='online', learning_offset=50.,
                                     random_state=0).fit(tf)
+    t1 = time.time()
+    print("Seconds for LDA: %.3f"%(t1 - t0))
+
+
 
     # Display topics
     su.display(lda,tf_feature_names,10)
@@ -35,8 +40,12 @@ def runNMF(documents,vocab_size,numTopics):
     tfidf = tfidf_vectorizer.fit_transform(documents)
     tfidf_feature_names = tfidf_vectorizer.get_feature_names()
     # Running
-    print("Running NMF")
+    print("Running Non-negative Matrix Factorization")
+    t0 = time.time()
     nmf = NMF(n_components=numTopics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf)
+    t1 = time.time()
+    print("Seconds for NMF: %.3f"%(t1 - t0))
+
 
     # Display topics
     su.display(nmf, tfidf_feature_names, 10)
