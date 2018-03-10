563a81aa3094bfb0cbe003cc26277d0e5caa9b74
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2018-03-10 15:45:56 -0500

    Display topics

--- Code/shared_utils.py
+++ Code/shared_utils.py
@@ -5,3 +5,10 @@ def vocab(dir,output_path):
     :param dir:
     :return:
     """
+    pass
+
+def display(model, feature_names, num_to_display):
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic %d:" % (topic_idx))
+        print(" ".join([feature_names[i]
+                        for i in topic.argsort()[:-num_to_display - 1:-1]]))
