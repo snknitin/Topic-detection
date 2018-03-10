81ee9f69a0750b7e89f74f98d3c1e7d36592fee4
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2018-03-10 15:46:26 -0500

    Models completed

--- Code/models.py
+++ Code/models.py
@@ -2,19 +2,41 @@
 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
-
+import shared_utils as su
 
 def runLDA(documents,vocab_size,numTopics):
+    """
+
+    :param documents:
+    :param vocab_size:
+    :param numTopics:
+    :return:
+    """
     tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=vocab_size, stop_words='english')
     tf = tf_vectorizer.fit_transform(documents)
     tf_feature_names = tf_vectorizer.get_feature_names()
 
     # Running
+    print("Running LDA")
     lda = LatentDirichletAllocation(n_topics=numTopics, max_iter=5, learning_method='online', learning_offset=50.,
                                     random_state=0).fit(tf)
 
+    # Display topics
+    su.display(lda,tf_feature_names,10)
 
 def runNMF(documents,vocab_size,numTopics):
+    """
+    :param documents:
+    :param vocab_size:
+    :param numTopics:
+    :return:
+    """
     tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=vocab_size, stop_words='english')
     tfidf = tfidf_vectorizer.fit_transform(documents)
     tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+    # Running
+    print("Running NMF")
+    nmf = NMF(n_components=numTopics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf)
+
+    # Display topics
+    su.display(nmf, tfidf_feature_names, 10)
